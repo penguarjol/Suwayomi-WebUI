acb48a04846660c14e65c4e4dcda153fe7909cee
Arjol Pengu <apengu@nuvalence.io>
2026-04-05 00:34:33 -0400

    feat(mobile): Initialize Capacitor and RevenueCat wrappers

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tachiyomi.saas',
+  appName: 'Suwayomi-SaaS',
+  webDir: 'dist'
+};
+
+export default config;
